0674b00412bfe473ddb99042d1470da0b6200e8e
admin <zolotukhinpv@gmail.com>
2023-12-10 00:00:56 +0200

    prepared page template

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {ImageModule} from "primeng/image";
+import {BreadcrumbModule} from "primeng/breadcrumb";
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    ImageModule,
+    BreadcrumbModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent  implements OnInit{
+
+  title:string = "Mississippi";
+  logoImgPath: string="/assets/image/logo.png";
+
+  items: MenuItem[] | undefined;
+
+  home: MenuItem | undefined;
+
+  ngOnInit(): void {
+    this.items = [{ label: 'Computer' }, { label: 'Notebook' }, { label: 'Accessories' }, { label: 'Backpacks' }, { label: 'Item' }];
+
+    this.home = { icon: 'pi pi-home', routerLink: '/' };
+  }
+
+}
