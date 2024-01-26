155cebbdad37fa545243b897e861fe9bbd9513a1
admin <zolotukhinpv@gmail.com>
2024-01-26 21:39:39 +0200

    ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {SidebarComponent} from "./sidebar/sidebar.component";
 import {ContentComponent} from "./content/content.component";
@@ -8,12 +8,16 @@ import {FooterComponent} from "./footer/footer.component";
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, SidebarComponent, ContentComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet,
+      HeaderComponent, SidebarComponent, ContentComponent, FooterComponent,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mss-angular';
+    title = 'mss-angular';
+
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,21 +2,25 @@ import {Component, OnInit} from '@angular/core';
 import {ImageModule} from "primeng/image";
 import {BreadcrumbModule} from "primeng/breadcrumb";
 import {MenuItem} from "primeng/api";
+import {LogoComponent} from "./logo/logo.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     ImageModule,
-    BreadcrumbModule
+    BreadcrumbModule,
+    LogoComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent  implements OnInit{
 
-  title:string = "Mississippi";
-  logoImgPath: string="/assets/image/logo.png";
+  title:string = "Metro.iD";
+  // logoImgPath: string="/assets/image/mid_logo.png";
+  logoImgPath: string="/assets/svg/mid_logo_2.svg";
+  // logoImgPath: string="/assets/svg/logo.svg";
 
   items: MenuItem[] | undefined;
 
@@ -24,7 +28,6 @@ export class HeaderComponent  implements OnInit{
 
   ngOnInit(): void {
     this.items = [{ label: 'Computer' }, { label: 'Notebook' }, { label: 'Accessories' }, { label: 'Backpacks' }, { label: 'Item' }];
-
     this.home = { icon: 'pi pi-home', routerLink: '/' };
   }
 
