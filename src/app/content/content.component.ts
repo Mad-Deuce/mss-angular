dd904438b8c629664c9aba3ec09ea645f615a881
admin <zolotukhinpv@gmail.com>
2024-02-10 23:47:11 +0200

    add tabView

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TabViewModule} from "primeng/tabview";
+import {NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet} from "@angular/common";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [
+    TabViewModule,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgTemplateOutlet,
+    NgForOf,
+  ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
 export class ContentComponent {
 
+  activeTabIndex: number = 0;
+  tabs: TabNode[] = [new TabNode(),new TabNode(),new TabNode(), new TabNode(), new TabNode(),   new TabNode(), new TabNode(), new TabNode(), new TabNode(), new TabNode()];
+  context = {tabNode: new TabNode()};
+
+  removeTab(index: number) {
+    console.log(index);
+  }
+
+  activeIndexChange(index: number) {
+    console.log(index);
+  }
+}
+
+
+export class TabNode {
+
+  public id: string = "";
+  public label: string = "";         // displaying in the tab label
+  public tabHeader: string = "";        // displaying in the tab header
+
+  constructor() {
+    this.id = "11";
+    this.label = "defaultTabLabel";
+    this.tabHeader = "defaultTabHeader";
+  }
 }
